f3b17bbd3fe63f351dcdf752f2d1c019d509437d
Srikant301201 <145249711+Srikant301201@users.noreply.github.com>
2025-07-10 12:13:49 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,7 +3,7 @@
 // console.log(typeof score);
 // console.log(typeof (score));
 
-// let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 // console.log(valueInNumber);
 
 // "30" => 33
@@ -19,11 +19,51 @@
 // "" => false 
 // "srikanta" => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  *********************** Operations ***********************
+
+let value = 3
+
+let netvalue = -value
+//console.log(netvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 =  " tapas"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+// link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
 
 
 
