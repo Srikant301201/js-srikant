49a4181475a6d9a47f9bd7331ed26d2e556b6195
Srikant301201 <145249711+Srikant301201@users.noreply.github.com>
2025-07-10 08:28:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "30abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "30" => 33
+// "30abc" => NaN
+// True =>  1;  False => 0
+
+//let isLoggedIn = 1
+
+//let  booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "srikanta" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
