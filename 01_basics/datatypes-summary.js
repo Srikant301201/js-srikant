c62c78a819a701cd1110ed829cc4f9732f502f5b
Srikant301201 <145249711+Srikant301201@users.noreply.github.com>
2025-07-13 09:09:27 +0000

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -37,3 +37,41 @@ console.log(typeof bigNumber);
 
 // link to study 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+// Stack memory (Primitive ) = main use hota hai.
+
+let myName = "srikantsethidotcom"
+
+let anothername = myName
+anothername = "sethisrikant2001dotcom"
+
+console.log(myName);
+console.log(anothername);
+
+//Heap memory  (Non Primitive /Reference) = main use hoti hai.
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"    
+}
+
+let userTwo = userOne
+
+userTwo.email ="srikant@google.com"
+userTwo.upi ="user@ibl"
+
+console.log(userOne.email);
+console.log(userOne.upi);
+
+console.log(userTwo.email);
+console.log(userTwo.upi);
+
+
+
+
+
+
